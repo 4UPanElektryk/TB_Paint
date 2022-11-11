c7fae27bb99f8834a4b0adef5c80e1b57c6678ba
4UPanElektryk <80508394+4UPanElektryk@users.noreply.github.com>
2022-11-11 17:21:48 +0100

    updated buildscript

--- Release.bat
+++ Release.bat
@@ -1,3 +1,4 @@
+@echo off
 COPY ".\TB_Paint\bin\Release\TB_Paint.exe" ".\ToRelease\TB_Paint.exe"
 COPY ".\TB_Paint\bin\x64\Release\TB_Paint.exe" ".\ToRelease\TB_Paint_x64.exe"
 
@@ -6,3 +7,10 @@ COPY ".\TB_PaintCompactor\bin\x64\Release\TB_PaintCompactor.exe" ".\ToRelease\TB
 
 COPY ".\TB_PaintRuntime\bin\Release\TB_PaintRuntime.exe" ".\ToRelease\TB_PaintRuntime.exe"
 COPY ".\TB_PaintRuntime\bin\x64\Release\TB_PaintRuntime.exe" ".\ToRelease\TB_PaintRuntime_x64.exe"
+
+for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
+set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
+set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
+
+set "fullstamp=%YYYY%-%MM%-%DD%_%HH%-%Min%-%Sec%"
+echo %fullstamp% > ".\ToRelease\PackedOn.txt"
