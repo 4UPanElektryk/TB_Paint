03b890afe019857ba165c12d7f7e8b379a4dd864
4UPanElektryk <80508394+4UPanElektryk@users.noreply.github.com>
2022-11-11 13:43:09 +0100

    fixed typo in naming

---
+++ Release.bat
@@ -0,0 +1,8 @@
+COPY ".\TB_Paint\bin\Release\TB_Paint.exe" ".\ToRelease\TB_Paint.exe"
+COPY ".\TB_Paint\bin\x64\Release\TB_Paint.exe" ".\ToRelease\TB_Paint_x64.exe"
+
+COPY ".\TB_PaintCompactor\bin\Release\TB_PaintCompactor.exe" ".\ToRelease\TB_PaintCompactor.exe"
+COPY ".\TB_PaintCompactor\bin\x64\Release\TB_PaintCompactor.exe" ".\ToRelease\TB_PaintCompactor_x64.exe"
+
+COPY ".\TB_PaintRuntime\bin\Release\TB_PaintRuntime.exe" ".\ToRelease\TB_PaintRuntime.exe"
+COPY ".\TB_PaintRuntime\bin\x64\Release\TB_PaintRuntime.exe" ".\ToRelease\TB_PaintRuntime_x64.exe"
